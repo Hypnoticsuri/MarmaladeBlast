8bcabead83f3247bb1467653e12223c8b1ddfcd2
Hypno <146860564+Hypnoticsuri@users.noreply.github.com>
2023-12-08 10:01:13 -0800

    More Progress

--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnwer : MonoBehaviour
+public class DetectCollision : MonoBehaviour
 {
-    public GameObject[] enemyPrefabs;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +14,11 @@ public class EnemySpawnwer : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
     }
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public GameObject[] spawnPoints;
+    public int enemyIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void SpawnEnemy()
+    {
+       int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        Transform spawnPos = spawnPoints[Random.Range(0, spawnPoints.Length)].transform;
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos.position, spawnPos.rotation);
+    }
+}
